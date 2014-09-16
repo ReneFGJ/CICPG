fa5ca9a9ad8b5e79ded8d8e2342a56d98d4092d4
mariliaferreira <mariliaferreiraa@gmail.com>
2014-09-16 11:00:54 -0300

    Novo layout pagina "programação"

--- page_09.php
+++ page_09.php
@@ -1,154 +1,72 @@
 <div id="page09" class="page_min pg09">
-	<A HREF="index.php#page01"><img src="img/icone_top.jpg" align="right" border=0 class="top" title="retorna ao menu"></A>
 	<h1><span>Programação</span></h1>
 
-	<table style="width: 70%; margin-left: 15%;">
-	  <tr>
-	    <td>horários</td>
-	    <td>4 de novembro</td>
-	    <td>5 de novembro</td>		
-	    <td>6 de novembro</td>
-	  </tr>
+	<div id="programacao-info">
+		<div class="programacao-horario">
+			<div class="data-programacao cor-data-programacao_1">
+				<h5>4 de novembro</h5>
+			</div>
+			<div class="estilo_data4_1 celula-tabela-horario">
+				<p><span class="estilo-horario">9h00</span> - <strong>Abertura</strong></p>
+			</div>
+			<div class="estilo_data4_2 celula-tabela-horario">
+				<p><span class="estilo-horario">14h00</span> - <strong>Palestra 1</strong> com Dr. Carlos Silla, Professor da Universidade Tecnológica Federal do Paraná - Desvendando os Caminhos da Pós-Graduação no Brasil e no Exterior</p>
+				<p><span class="estilo-horario">14h00</span> - <strong>Mini Curso</strong> com Dr Milton Pires Ramos, Coordenador de IC da TECPAR - Redação Científica</p>
 
-	   <tr>
-	    <td>8h00</td>
-	    <td>Abertura</td>		
-	    <td>.</td>
-	    <td>.</td>
-	  </tr>
+			</div>
+			<div class="estilo_data4_3 celula-tabela-horario">
+				<p><span class="estilo-horario">16h20</span> - <strong>Palestra 2</strong> com Dr. Alireza Mohebi Ashtiani, Professor da Universidade Tecnológica Federal do Paraná - Optimização e suas aplicações</p>
+			</div>
+			<div class="estilo_data4_4 celula-tabela-horario">
+				<p><span class="estilo-horario">19h00</span> - <strong>Palestra 3</strong> com Dr. Alexandre Feldman, Professor da Universidade Tecnológica Federal do Paraná - Ciência e Religião: caminhos paralelos de conhecimento, ética e moral</p>
+			</div>
+		</div>
 
-	  <tr>
-	    <td>9h00</td>
-	    <td>Palestra 4 com Dr. Eduardo Filgueiras Damasceno, Professor da Universidade Tecnológica Federal do Paraná - Pesquisa em Computação, Saúde Física e Reabilitação</td>		
-	    <td>Mesa Redonda 2 A definir Palestrante e Mediadores - Tema: O legado da Copa</td>
-	    <td>.</td>
-	  </tr>
+		<div class="programacao-horario">
+			<div class="data-programacao cor-data-programacao_2">
+				<h5>5 de novembro</h5>
+			</div>
+			<div class="estilo_data5_1 celula-tabela-horario">
+				<p><span class="estilo-horario">9h00</span> - <strong>Palestra 4</strong> com Dr. Eduardo Filgueiras Damasceno, Professor da Universidade Tecnológica Federal do Paraná - Pesquisa em Computação, Saúde Física e Reabilitação</p>
+				<p><span class="estilo-horario">9h00</span> - <strong>Palestra 5</strong> com Dr. Marco Krieger, FioCruz - NanoSUS - da pesquisa básica a novos produtos</p>
 
-	  <tr>
-	  	<td>10h20</td>
-	  	<td>.</td>
-	  	<td>Mini Curso com Dr Milton Pires Ramos, Coordenador de IC da TECPAR - Redação Científica<br /> - Sessão de apresentação do programa Ciências sem Fronteira(CSF)</td>
-	  	<td>Oficina 1 com A definir Palestrante - Captação de recursos para o pesquisador <br /> - Mini Curso com Dr Milton Pires Ramos, Coordenador de IC da TECPAR - Minicurso Redação Científica</td>
-	  </tr>
+			</div>
+			<div class="estilo_data5_2 celula-tabela-horario">
+				<p><span class="estilo-horario">10h20</span> - <strong>Mini Curso</strong> com Dr Milton Pires Ramos, Coordenador de IC da TECPAR - Redação Científica</p>
+				<p><span class="estilo-horario">10h20</span> - Sessão de apresentação do programa <strong>Ciências sem Fronteiras</strong> (CsF)</p>
 
-	  <tr>
-	    <td>14h00</td>
-	    <td>- Palestra 1 com Dr. Carlos Silla, Professor da Universidade Tecnológica Federal do Paraná - Desvendando os Caminhos da Pós-Graduação no Brasil e no Exterior<br /> - Mini Curso com Dr Milton Pires Ramos, Coordenador de IC da TECPAR - Redação Científica</td>		
-	    <td>- Palestra 6 com Dr. Carlos Eduardo Marquioni, Professor da Universidade Tuiuti do Paraná - as transformações na prática jornalística trazidas pela internet, dispositivos móveis e mídias sociais</td>
-	  	<td>- Oficina 2 com Dr. Roberto Pecoits, Professor da PUCPR - Oficina sobre a formação do pesquisador <br /> - Palestra 7 A definir <br /> - Oficina 3 com Dr Alvaro Amarante, Professor da PUCPR - Ciência, tecnologia e inovação</td>
-	  </tr>
+			</div>
+			<div class="estilo_data5_3 celula-tabela-horario">
+				<p><span class="estilo-horario">14h00</span> - <strong>Palestra 6</strong> com Dr. Carlos Eduardo Marquioni, Professor da Universidade Tuiuti do Paraná - as transformações na prática jornalística trazidas pela internet, dispositivos móveis e mídias sociais</p>
+			</div>
+			<div class="estilo_data5_4 celula-tabela-horario">
+				<p><span class="estilo-horario">16h20</span> - <strong>Mesa Redonda 1</strong> com Ms. Palmira Sevegnani, Professor da Universidade Federal do Paraná - Pesquisa interdisciplinar: Arte e Cultura. Mediadores: Silvia Monteiro e Adalgisa Oliveira</p>
+			</div>
+			
+		</div>
 
-	  <tr>
-	    <td>16h20</td>
-	    <td>- Palestra 2 com Dr. Alireza Mohebi Ashtiani, Professor da Universidade Tecnológica Federal do Paraná - Optimização e suas aplicações</td>		
-	    <td>- Mesa Redonda 1 com Ms. Palmira Sevegnani, Professor da Universidade Federal do Paraná - Pesquisa interdisciplinar: Arte e Cultura. Mediadores: Silvia Monteiro e Adalgisa Oliveira</td>
-	  	<td>.</td>
-	  </tr>
+		<div class="programacao-horario">
+			<div class="data-programacao cor-data-programacao_3">
+				<h5>6 de novembro</h5>
+			</div>
+			<div class="estilo_data6_1 celula-tabela-horario">
+				<p><span class="estilo-horario">8h00</span> - <strong>Mesa Redonda 2</strong> <i>A definir Palestrante e Mediadores</i> - Tema: O legado da Copa</p>
+			</div>
+			<div class="estilo_data6_2 celula-tabela-horario">
+				<p><span class="estilo-horario">10h20</span> - <strong>Oficina 1</strong> <i>A definir Palestrante</i> - Captação de recursos para o pesquisador</p>
+				<p><span class="estilo-horario">10h20</span> - <strong>Mini Curso</strong> com Dr Milton Pires Ramos, Coordenador de IC da TECPAR - Minicurso Redação Científica</p>
 
-	  <tr>
-	  	<td>17h00</td>
-	  	<td>.</td>
-	  	<td>.</td>
-	  	<td>Encerramento</td>
-	  </tr>
+			</div>
+			<div class="estilo_data6_3 celula-tabela-horario">
+				<p><span class="estilo-horario">14h00</span> - <strong>Oficina 2</strong> com Dr. Roberto Pecoits, Professor da PUCPR - Oficina sobre a formação do pesquisador</p>
+				<p><span class="estilo-horario">14h00</span> - <strong>Palestra 7</strong> <i>A definir</i></p>
+				<p><span class="estilo-horario">14h00</span> - <strong>Oficina 3</strong> com Dr Alvaro Amarante, Professor da PUCPR - Ciência, tecnologia e inovação </p>
 
+			</div>
+			<div class="estilo_data6_4 celula-tabela-horario">
+				<p><span class="estilo-horario">17h00</span> - <strong>Encerramento</strong></p>
+			</div>
+		</div>
+	</div>
 
-	</table>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!--
-	<table width="800" align="center">
-		<TD colspan=3 style="border-top: 1px solid #334333; padding-top: 30px;" class="lt3">4 Novembro 2014</TD>
-		</TR>
-		<TR>
-			<TD width="40">09h00</TD><TD>Abertura</TD>
-		</TR>
-		<TR>
-			<TD width="40">14h00</TD><TD>Palestra 1 com Dr. Carlos Silla, Professor da Universidade Tecnológica Federal do Paraná - Desvendando os Caminhos da Pós-Graduação no Brasil e no Exterior
-			<br>
-        </TR>
-        <TR>
-            </TD>
-            <TD width="40">14h00</TD><TD>Mini Curso com Dr Milton Pires Ramos, Coordenador de IC da TECPAR - Redação Científica
-			<br>
-            </TR>
-            </TD>
-		</TR>
-		<TR>
-			<TD width="40">16h20</TD><TD>Palestra 2 com Dr. Alireza Mohebi Ashtiani, Professor da Universidade Tecnológica Federal do Paraná - Optimização e suas aplicações</TD>
-		</TR>
-		<TR>
-			<TD width="40">19h00</TD><TD>Palestra 3 com Dr. Alexandre Feldman, Professor da Universidade Tecnológica Federal do Paraná - Ciência e Religião: caminhos paralelos de conhecimento, ética e moral</TD>
-		</TR>
-		<TR>
-			<TD colspan=3 style="border-top: 1px solid #334333; padding-top: 30px;" class="lt3">5 Novembro 2014</TD>
-		</TR>
-		<TR valign="top">
-			<TD width="40">09h00</TD><TD>Palestra 4 com Dr. Eduardo Filgueiras Damasceno, Professor da Universidade Tecnológica Federal do Paraná - Pesquisa em Computação, Saúde Física e Reabilitação</TD>
-		</TR>
-          <TR>
-			<TD width="40">09h00</TD><TD>Palestra 5 com Dr. Marco Krieger, FioCruz - NanoSUS - da pesquisa básica a novos produtos.</TD>
-		</TR>
-        <TR>
-			<TD width="40">10h20</TD><TD>Mini Curso com Dr Milton Pires Ramos, Coordenador de IC da TECPAR - Redação Científica</TD>
-		</TR>
-        	<TR valign="top">
-			<TD width="40">10h20</TD><TD>Sessão de apresentação do programa Ciências sem Fronteira(CSF)</TD>
-		</TR>
-		<TR valign="top">
-			<TD width="40">14h00</TD><TD>Palestra 6 com Dr. Carlos Eduardo Marquioni, Professor da Universidade Tuiuti do Paraná - as transformações na prática jornalística trazidas pela internet, dispositivos móveis e mídias sociais </TD>
-		</TR>
-		<TR valign="top">
-			<TD width="40">16h20</TD><TD>Mesa Redonda 1 com Ms. Palmira Sevegnani, Professor da Universidade Federal do Paraná - Pesquisa interdisciplinar: Arte e Cultura. Mediadores: Silvia Monteiro e Adalgisa Oliveira</TD>
-			<BR>
-		</TR>
-		<TR>
-			<TD colspan=3 style="border-top: 1px solid #334333; padding-top: 30px;" class="lt3">6 Novembro 2014</TD>
-		</TR>
-       		<TR>
-			<TD width="40">08h00</TD><TD>Mesa Redonda 2   A definir Palestrante e Mediadores - Tema: O legado da Copa </TD>
-		</TR>
-                  <TR>
-			<TD width="40">10h20</TD><TD>Oficina 1 com A definir Palestrante - Captação de recursos para o pesquisador</TD>
-		</TR>
-         <TR>
-			<TD width="40">10h20</TD><TD>Mini Curso com Dr Milton Pires Ramos, Coordenador de IC da TECPAR - Minicurso Redação Científica</TD>
-		</TR>
-        <TR>
-			<TD width="40">14h00</TD><TD>Oficina 2 com Dr. Roberto Pecoits,  Professor da PUCPR - Oficina sobre a formação do pesquisador</TD>
-		</TR>
-        <TR>
-			<TD width="40">14h00</TD><TD>Palestra 7 A definir </TD>
-		</TR>
-		<TR>
-			<TD width="40">14h00</TD><TD>Oficina 3 com Dr Alvaro Amarante, Professor da PUCPR - Ciência, tecnologia e inovação
-			<BR>
-			</TD>
-			<BR>
-		</TR>
-		<BR>
-		<TR>
-			<TD>17h00</TD><TD>Encerramento</TD>
-		</TR>
-		<BR>
-		<BR>
-		<TR>
-
-	</table> -->
-	<BR><BR><BR>
 </div>
